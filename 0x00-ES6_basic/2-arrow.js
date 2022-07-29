6e88e817a4be6a2cd5fa7a073acadb2f834e8008
Luis Mejia <luismejia69@gmail.com>
2022-07-29 16:46:30 -0500

    arrows functions

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,19 @@
+/*export default function getNeighborhoodsList() {
+  this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = function add(newNeighborhood) {
+    self.sanFranciscoNeighborhoods.push(newNeighborhood);
+    return self.sanFranciscoNeighborhoods;
+  };
+}*/
+
+
+export default function getNeighborhoodsList() {
+    this.sanFranciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+    this.addNeighborhood =   (newNeighborhood) =>  { 
+    this.sanFranciscoNeighborhoods.push(newNeighborhood)
+    return this.sanFranciscoNeighborhoods
+    }
+}
