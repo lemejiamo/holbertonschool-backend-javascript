20e850fe0d3e0b874cad3f00ef78ec14b02a7a76
Luis Mejia <luismejia69@gmail.com>
2022-07-31 18:03:15 -0500

    pricing

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,46 @@
+import Currency from "./3-currency";
+
+export default class Pricing {
+  constructor(amount = 0, currency = '') {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  /**
+   * @param {number} name
+   */
+  set amount(amount) {
+    if (typeof (amount) === 'number') {
+      this._amount = amount;
+    } else {
+      throw new TypeError('Amount must be a number');
+    }
+  }
+
+  /**
+   * @param {Currency} code
+   */
+  set currency(currency) {
+    if (currency instanceof Currency) {
+      this._currency = currency;
+    } else {
+      throw new TypeError('Currency must be a instance of Currency class');
+    }
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  displayFullPrice() {
+    return `${this._amount} ${this.currency.name} (${this.currency.code})`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
