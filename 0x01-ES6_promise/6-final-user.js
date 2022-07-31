b97499516dd134d7666a6321cd49ba68c193d18f
Luis Mejia <luismejia69@gmail.com>
2022-07-31 12:25:51 -0500

    resolve bad import signUpUser

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,9 +1,11 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, filename) {
+function handleProfileSignup(firstName, lastName, filename) {
   const user = signUpUser(firstName, lastName);
   const photo = uploadPhoto(filename);
   return Promise.all([user, photo])
     .then((data) => [{ status: data.status, value: data.value }]);
 }
+
+export default handleProfileSignup;
