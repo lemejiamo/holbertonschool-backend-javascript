bef873c13567d8fa6cdf2b9035e0b5a5f7f89bf2
Luis Mejia <luismejia69@gmail.com>
2022-07-31 12:21:08 -0500

    revision

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,3 +1,12 @@
-export default function uploadPhoto(filename) {
-  return new Promise.reject(new Error(`${filename} cannot be processed`));
+/* export default function uploadPhoto(filename) {
+  return new Promise((reject) => {
+    reject(new Error(`${filename} cannot be processed`));
+  });
 }
+ */
+
+function uploadPhoto(filename) {
+  return Promise.reject(new Error(`${filename} cannot be processed`));
+}
+
+export default uploadPhoto;
