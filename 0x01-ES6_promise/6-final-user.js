d9e4575b336943302bd6f86b1037f27c9960c022
Luis Mejia <luismejia69@gmail.com>
2022-07-31 13:02:43 -0500

    final user

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,11 +1,35 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-function handleProfileSignup(firstName, lastName, filename) {
-  const user = signUpUser(firstName, lastName);
-  const photo = uploadPhoto(filename);
-  return Promise.all([user, photo])
-    .then((data) => [{ status: data.status, value: data.value }]);
+async function handleProfileSignup(firstName, lastName, filename) {
+  const statusList = [];
+
+  try {
+    const user = await signUpUser(firstName, lastName);
+    statusList.push({
+      status: 'resolved',
+      value: user,
+    });
+  } catch (err) {
+    statusList.push({
+      status: 'rejected',
+      value: err.toString(),
+    });
+  }
+
+  try {
+    const photo = await uploadPhoto(filename);
+    statusList.push({
+      status: 'resolved',
+      value: photo,
+    });
+  } catch (err) {
+    statusList.push({
+      status: 'rejected',
+      value: err.toString(),
+    });
+  }
+  return statusList;
 }
 
 export default handleProfileSignup;
