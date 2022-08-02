a681b90e1783db055563face0c153e12dcddcf5e
Luis Mejia <luismejia69@gmail.com>
2022-08-02 17:13:21 -0500

    simple list

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,23 @@
+export default function getListStudents() {
+  const allStudentsList = [
+    {
+      id: 1,
+      firstname: 'Guillaume',
+      location: 'San Fransisco',
+    },
+
+    {
+      id: 2,
+      firstname: 'James',
+      location: 'Columbia',
+    },
+
+    {
+      id: 3,
+      firstname: 'Serena',
+      location: 'San Fransisco',
+    },
+  ];
+
+  return allStudentsList;
+}
