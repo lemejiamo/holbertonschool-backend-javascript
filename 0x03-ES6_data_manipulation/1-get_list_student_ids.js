507a9b6659a6bc33c5cf2e45a123adfbde3d3cf2
Luis Mejia <luismejia69@gmail.com>
2022-08-02 17:47:16 -0500

    list by id

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,10 @@
+function getListStudentIds(studentsList) {
+  if (studentsList instanceof Array){
+    // console.log('es instancia')
+    const idList = studentsList.map(student => student.id);
+    return idList;
+  }
+  return [];
+}
+
+export default getListStudentIds;
