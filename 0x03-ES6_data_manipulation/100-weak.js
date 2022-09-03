b4729db24bc2b023cdd5f8610bc11dcfb4a349b1
Luis Mejia <luismejia69@gmail.com>
2022-09-03 16:25:32 -0500

    weakMap

--- 0x03-ES6_data_manipulation/100-weak.js
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -1,4 +1,8 @@
-const weakMap = WeakMap()
-export default function queryAPI(protocol, name){
-  
-}
+export const weakMap = new WeakMap();
+
+export default function queryAPI(endpoint) {
+    let called = weakMap.get(endpoint) || 0;
+    called += 1;
+    if (called >= 5) throw new Error('Endpoint load is high');
+    weakMap.set(endpoint, called);
+  }
